5ffe1f50e7e13780a14bec56db048957ebfa4568
Your Name <name@xyz.com>
2025-10-23 13:48:33 +0800

    1.4

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import plusOperation
+import minusOperation
+
 a = int(input("enter a:"))
 b = int(input("enter b:"))
 op = input("enter an operation:")
 
 if (op == "+"):
     answer = plusOperation.plus(a,b)
-    print(f"answer is {answer}")
+    
+
+if (op == "-"):
+    answer = minusOperation.minus(a,b)
+    
+print(f"answer is {answer}")
---
+++ minusOperation.py
@@ -0,0 +1,2 @@
+def minus(a,b):
+    return (a - b)    #这是减法函数，输出两个数相减的值
